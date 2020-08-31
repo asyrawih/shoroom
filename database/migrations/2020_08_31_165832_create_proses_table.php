978bd5cf4f06db249d3e34005690376f2ac51e94
luck_nut <hasyrawi@gmail.com>
2020-09-01 01:02:08 +0800

    create migration prosess

---
+++ database/migrations/2020_08_31_165832_create_proses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('customer_id');
+            $table->foreignId('plant_id');
+            $table->foreignId('unit_id');
+            $table->string('no_unit');
+            $table->text('lokasi_unit');
+            $table->string('kota');
+            $table->string('hoo');
+            $table->string('smu');
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proses');
+    }
+}
