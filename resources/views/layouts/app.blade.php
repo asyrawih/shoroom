1509bffc2e62a6010f05d38cee18894fe175296a
luck_nut <hasyrawi@gmail.com>
2020-09-02 05:03:42 +0800

    tweaks welcome =

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,32 +12,36 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body class="bg-main h-screen antialiased leading-none">
     <div id="app">
-        <nav class="bg-main shadow mb-8 py-6 shadow-lg">
+        <nav class="bg-main  mb-8 py-6 shadow-lg">
             <div class="container mx-auto px-6 md:px-0">
                 <div class="flex items-center justify-center">
                     <div class="mr-6">
-                        <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                        <a href="{{ url('/') }}" class="text-lg font-bold shadow text-secondary no-underline">
                             {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
                     <div class="flex-1 text-right">
                         @guest
-                            <a class="no-underline hover:underline font-bold text-gray-100 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            {{-- <a
+                                class="no-underline hover:underline font-bold text-gray-100 text-sm p-3"
+                                href="{{ route('login') }}">{{ __('Login') }}</a>
                             @if (Route::has('register'))
-                                <a class="no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            @endif
+                                <a class="no-underline hover:underline text-gray-300 text-sm p-3"
+                                    href="{{ route('register') }}">{{ __('Register') }}</a>
+                            @endif --}}
                         @else
-                            <span class="text-gray-300 font-bold text-sm pr-4">{{ auth()->user()->name }}( {{ auth()->user()->jabatan }} )</span>
-                            <a href="{{ route('logout') }}"
-                               class="no-underline hover:underline text-gray-300 text-sm p-3"
-                               onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                            <span class="text-gray-300 font-bold text-sm pr-4">{{ auth()->user()->name }}(
+                                {{ auth()->user()->jabatan }} )</span>
+                            <a href="{{ route('logout') }}" class="no-underline hover:underline text-gray-300 text-sm p-3"
+                                onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                                 {{ csrf_field() }}
                             </form>
@@ -48,4 +53,5 @@
         @yield('content')
     </div>
 </body>
+
 </html>
